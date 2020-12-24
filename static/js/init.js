439a5fd6cc4e5f7e2839734908fffb147ca2bb74
Liu Ziang <39642528+AceSix@users.noreply.github.com>
2020-12-24 17:36:47 +0800

    Init

---
+++ static/js/init.js
@@ -0,0 +1,8 @@
+/*
+ * @FilePath: \AceSix.github.io\static\js\init.js
+ * @Author: Ziang Liu
+ * @Date: 2020-11-03 22:27:47
+ * @LastEditors: Ziang Liu
+ * @LastEditTime: 2020-12-24 17:34:16
+ * @Copyright (C) 2020 SJTU. All rights reserved.
+ */
