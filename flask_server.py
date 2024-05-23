6bfb38fb431c2fd903df3b270e1eac965846dce7
Alex <39642528+AceSix@users.noreply.github.com>
2024-05-23 15:45:39 -0400

    Add eco map view

---
+++ flask_server.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+###################################################################
+###   @FilePath: \AceSix.github.io\flask_server.py
+###   @Author: AceSix
+###   @Date: 2024-05-23 14:23:46
+###   @LastEditors: AceSix
+###   @LastEditTime: 2024-05-23 14:32:02
+###   @Copyright (C) 2024 Brown U. All rights reserved.
+###################################################################
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, render_template
+
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+def index():
+    return render_template('eco_maps.html')
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
